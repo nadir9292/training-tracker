4b65a628c406045e4e079c0e9449fd29e6326c44
Nadir Mansouri <61685527+nadir9292@users.noreply.github.com>
2024-09-20 15:08:19 +0200

    update firebase admin env

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,13 +1,23 @@
 import admin from 'firebase-admin'
 
 if (!admin.apps.length) {
+  const firebaseCredentialsBase64 =
+    process.env.FIREBASE_ADMIN_CREDENTIALS_BASE64
+
+  if (!firebaseCredentialsBase64) {
+    throw new Error('Env var : FIREBASE_ADMIN_CREDENTIALS_BASE64 is missing.')
+  }
+  const firebaseCredentials = JSON.parse(
+    Buffer.from(firebaseCredentialsBase64, 'base64').toString('utf-8')
+  )
+
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      projectId: firebaseCredentials.project_id,
+      clientEmail: firebaseCredentials.client_email,
+      privateKey: firebaseCredentials.private_key.replace(/\\n/g, '\n'),
     }),
-    databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: `https://${firebaseCredentials.projectId}.firebaseio.com`,
   })
 }
 
